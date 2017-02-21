b28332a58f6d50c48cda2c1494d74be35faad897
Spencer Lyon <spencerlyon2@gmail.com>
2017-02-21 17:54:36 -0500

    ENH: added return type, verbosity option

--- src/MINPACK.jl
+++ src/MINPACK.jl
@@ -9,6 +9,42 @@ function f!(x, fvec=similar(x))
     fvec
 end
 
+
+type AlgoData
+    n_calls::Int
+    show_trace::Bool
+
+    AlgoData(verbose=false) = new(0, verbose)
+end
+
+Base.unsafe_convert(::Type{Ptr{Void}}, o::AlgoData) = o
+
+immutable SolverResults
+    algo::String
+    initial_x::Vector{Float64}
+    x::Vector{Float64}
+    f::Vector{Float64}
+    return_code::Int
+    msg::String
+    obj::AlgoData
+end
+
+# NOTE: this method was adapted from NLsolve.jl
+function Base.show(io::IO, s::SolverResults)
+    @printf io "Results of Nonlinear Solver Algorithm\n"
+    @printf io " * Algorithm: %s\n" s.algo
+    @printf io " * Starting Point: %s\n" string(s.initial_x)
+    @printf io " * Zero: %s\n" string(s.x)
+    @printf io " * Inf-norm of residuals: %f\n" norm(s.f, Inf)
+    # @printf io " * Iterations: %d\n" r.iterations
+    @printf io " * Convergence: %s\n" s.return_code==1
+    @printf io " * Message: %s\n" s.msg
+    # @printf io "   * |x - x'| < %.1e: %s\n" r.xtol r.x_converged
+    # @printf io "   * |f(x)| < %.1e: %s\n" r.ftol r.f_converged
+    @printf io " * Function Calls: %d\n" s.obj.n_calls
+    # @printf io " * Jacobian Calls (df/dx): %d" r.g_calls
+end
+
 immutable ConvergenceError <: Exception
     msg::String
     return_code::Int
@@ -24,48 +60,50 @@ function _hybrd1_func_wrapper(_p::Ptr{Void}, n::Cint, _x::Ptr{Cdouble},
         return Cint(0)
     end
     f!(x, fvec)
+
+    obj = unsafe_pointer_to_objref(_p)::AlgoData
+    obj.n_calls += 1
+    if obj.show_trace
+        println("it: $(obj.n_calls)\terr: $(norm(fvec, Inf))")
+    end
+
     _hybrd1_func_ref[](x, fvec)
     Cint(0)
 end
 const _hybrd1_cfunc = cfunction(_hybrd1_func_wrapper, Cint, (Ptr{Void}, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Cint))
 
-function hybrd1(f!::Function, x0::Vector{Float64}; tol::Float64=1e-8)
+const _hybrd1_messages = Dict{Int,String}(
+    1 => "MINPACK says algorithm estimates that the relative error between x and the solution is at most tol",
+    2 => "MINPACK says maximum iterations has been exceeded",
+    3 => "MINPACK says tol is too small, no further improvement in x is possible",
+    4 => "MINPACK says iteration is not making good progress",
+    -1 => "MINPACK says user terminated iterations with code "
+)
+
+function hybrd1(f!::Function, x0::Vector{Float64}; tol::Float64=1e-8,
+                show_trace::Bool=false)
     x = copy(x0)
     fvec = similar(x)
     n = length(x)
     lwa = ceil(Int, (n*(3*n+13))/2)
     wa = ones(lwa)
     _hybrd1_func_ref[] = f!
+    obj = AlgoData(show_trace)
 
     return_code = ccall(
         (:hybrd1, cminpack),
         Cint,
         (Ptr{Void}, Ptr{Void}, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Cdouble, Ptr{Cdouble}, Cint),
-        _hybrd1_cfunc, &nothing, n, x, fvec, tol, wa, lwa
+        _hybrd1_cfunc, pointer_from_objref(obj), n, x, fvec, tol, wa, lwa
     )
 
-    # handle return code
-    if return_code == 0
-        msg = "MINPACK says Improper input parameters"
-        throw(ArgumentError(msg))
-    elseif return_code == 1
-        # success
-        nothing
-    elseif return_code == 2
-        msg = "MINPACK says maximum iterations has been exceeded"
-        throw(ConvergenceError(msg, return_code))
-    elseif return_code == 3
-        msg = "MINPACK says tol is too small, no further improvement in x is possible"
-        warn(msg)
-    elseif return_code == 4
-        msg = "MINPACK says iteration is not making good progress"
-        warn(msg)
-    elseif return_code < 0
-        msg = "MINPACK says user terminated iterations with code $(return_code)"
+    msg = _hybrd1_messages[max(-1, return_code)]
+    if return_code < 0
+        msg = msg * string(return_code)
         throw(ConvergenceError(msg, return_code))
     end
 
-    x, fvec
+    SolverResults("Modified Powell", x0, x, fvec, return_code, msg, obj)
 end
 
 const _lmdif1_func_ref = Array(Function)
@@ -78,13 +116,21 @@ function _lmdif1_func_wrapper(_p::Ptr{Void}, m::Cint, n::Cint, _x::Ptr{Cdouble},
         return Cint(0)
     end
     f!(x, fvec)
+
+    obj = unsafe_pointer_to_objref(_p)::AlgoData
+    obj.n_calls += 1
+    if obj.show_trace
+        println("it: $(obj.n_calls)\terr: $(norm(fvec, Inf))")
+    end
+
     _lmdif1_func_ref[](x, fvec)
     Cint(0)
 end
 const _lmdif1_cfunc = cfunction(_lmdif1_func_wrapper, Cint, (Ptr{Void}, Cint, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Cint))
 
 # NOTE: default doesn't always hold
-function lmdif1(f!::Function, x0::Vector{Float64}, m::Int=length(x0); tol::Float64=1e-8)
+function lmdif1(f!::Function, x0::Vector{Float64}, m::Int=length(x0); tol::Float64=1e-8,
+                show_trace::Bool=false)
     x = copy(x0)
     n = length(x)
     if n > m
@@ -97,12 +143,13 @@ function lmdif1(f!::Function, x0::Vector{Float64}, m::Int=length(x0); tol::Float
     iwa = Array{Int}(n)
     wa = ones(lwa)
     _lmdif1_func_ref[] = f!
+    obj = AlgoData(show_trace)
 
     return_code = ccall(
         (:lmdif1,cminpack),
         Cint,
         (Ptr{Void}, Ptr{Void}, Cint, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Cdouble, Ptr{Cint}, Ptr{Cdouble}, Cint),
-        _lmdif1_cfunc, &nothing, m, n, x, fvec, tol, iwa, wa, lwa
+        _lmdif1_cfunc, pointer_from_objref(obj), m, n, x, fvec, tol, iwa, wa, lwa
     )
 
     # handle return code
@@ -138,7 +185,7 @@ function lmdif1(f!::Function, x0::Vector{Float64}, m::Int=length(x0); tol::Float
         throw(ConvergenceError(msg, return_code))
     end
 
-    x, fvec
+    SolverResults("Levenberg-Marquardt", x0, x, fvec, return_code, msg, obj)
 end
 
 end  # module
